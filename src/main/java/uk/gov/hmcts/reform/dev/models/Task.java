61bbd23cd1ea626928dc96a8b95c77c880b368f3
dan@dopksi <hextra@live.com>
2025-04-13 13:07:08 +0100

    Add Task model with Lombok annotations

--- src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
+++ src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.dev.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +14,9 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     
     @Id
@@ -28,54 +35,11 @@ public class Task {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dueDate;
     
-    // Default constructor
-    public Task() {
-    }
-    
+    // Constructor without id for creating new tasks
     public Task(String title, String description, String status, LocalDateTime dueDate) {
         this.title = title;
         this.description = description;
         this.status = status;
-        this.dueDate = dueDate;
-    }
-    
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public String getDescription() {
-        return description;
-    }
-    
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
-    public String getStatus() {
-        return status;
-    }
-    
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-    
-    public void setDueDate(LocalDateTime dueDate) {
         this.dueDate = dueDate;
     }
 }
