35ffdf198d7a03963f3624c7b91292c13f32b684
dan@dopksi <hextra@live.com>
2025-04-13 17:35:04 +0100

    Updated dueData format

--- src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
+++ src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
@@ -16,7 +16,7 @@ public class Task {
     
     private String status;
     
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime dueDate;
     
     /**
